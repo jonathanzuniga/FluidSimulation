78d803dcf2c30ef8a9ef648551cea5c259909a59
Jonathan Zúñiga <jonathanzuniga@gmail.com>
2016-05-27 12:22:52 -0700

    Initial commit.

---
+++ FluidSimulation/FluidSimulation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace FluidSimulation
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			// The root page of your application
+			var template = new Fluid ();
+			var page = template.GenerateString ();
+			var html = new HtmlWebViewSource { Html = page };
+
+			// The root page of your application.
+			MainPage = new ContentPage {
+				Content = new WebView () {
+					Source = html
+				}
+			};
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
